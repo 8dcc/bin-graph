43c8712af6f1a5a943b406ca0f0daa9fb04eec9c
8dcc <8dcc.git@gmail.com>
2024-07-21 00:47:55 +0200

    Add more checks to bin-graph-sections.sh

--- bin-graph-section.sh
+++ bin-graph-section.sh
@@ -11,11 +11,19 @@ fi
 section=$1
 input=${@: -2:1}
 
-readelf_output=$(readelf --sections --wide $input | grep $section | head -n 1 | tr -s ' ')
-start_offset=$(echo "$readelf_output" | cut -d ' ' -f 6)
-section_end=$(echo "$readelf_output" | cut -d ' ' -f 7)
-end_offset=$(printf '%x' $((0x$start_offset + 0x$section_end + 1)))
+readelf_output=$(readelf --sections --wide "$input" | grep --fixed-strings "$section" | head -n 1 | tr -s ' ')
 
+if [ -z "$readelf_output" ]; then
+    echo "Section \"$section\" not found"
+    exit 1
+fi
+
+section_name=$(echo "$readelf_output" | cut -d ' ' -f 3)
+start_offset=$(echo "$readelf_output" | cut -d ' ' -f 6)
+section_size=$(echo "$readelf_output" | cut -d ' ' -f 7)
+end_offset=$(printf '%x' $((0x$start_offset + 0x$section_size + 1)))
+
+echo "Section name: $section_name"
 echo "Start offset: 0x$start_offset"
 echo "End offset: 0x$end_offset"
 
