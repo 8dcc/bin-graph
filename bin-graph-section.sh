e7f5a764fd3f32ba04aa254ba40eb2572cfd87e0
8dcc <8dcc.git@gmail.com>
2025-04-05 11:50:41 +0200

    Improve 'bin-graph-section.sh' script
    * Use '/usr/bin/env bash' instead of '/bin/sh'.
    * Show only basename in Usage.
    * Print error messages to 'stderr' instead of 'stdout'.
    * Quote arrays, and output of 'readelf'.
    * Don't store the entire 'bin-graph' command in a string, split into
      'cmd' and an 'args' list.

--- bin-graph-section.sh
+++ bin-graph-section.sh
@@ -1,34 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Parse the output of `readelf', and use the offsets of the specified section
 # as arguments for `bin-graph'.
 
 if [ $# -lt 3 ]; then
-    echo "Usage: $0 SECTION [OPTIONS...] INPUT OUTPUT.png"
+    echo "Usage: $(basename "$0") SECTION [OPTION...] INPUT OUTPUT.png" 1>&2
     exit 1
 fi
 
 input_section=$1
-input_file=${@: -2:1}
+input_file="${*: -2:1}"
 
-readelf_output=$(readelf --sections --wide "$input_file" | grep --fixed-strings "$input_section" | head -n 1 | tr -s ' ')
+readelf_output="$(readelf --sections --wide "$input_file" | grep --fixed-strings "$input_section" | head -n 1 | tr -s ' ')"
 
 if [ -z "$readelf_output" ]; then
-    echo "Section \"$input_section\" not found"
+    echo "$(basename "$0"): Section '${input_section}' not found." 1>&2
     exit 1
 fi
 
 section_name=$(echo "$readelf_output" | cut -d ' ' -f 3)
-echo "Section name: $section_name"
+echo "Section name: ${section_name}"
 
 offset_start_str=$(echo "$readelf_output" | cut -d ' ' -f 6)
-offset_start=$(printf '%x' "0x$offset_start_str")
-echo "Start offset: 0x$offset_start"
+offset_start=$(printf '%x' "0x${offset_start_str}")
+echo "Start offset: 0x${offset_start}"
 
 section_size=$(echo "$readelf_output" | cut -d ' ' -f 7)
-offset_end=$(printf '%x' $((0x$offset_start + 0x$section_size + 1)))
-echo "End offset: 0x$offset_end"
+offset_end=$(printf '%x' $((0x${offset_start} + 0x${section_size} + 1)))
+echo "End offset: 0x${offset_end}"
 
-bin_graph_cmd="./bin-graph --offset-start $offset_start --offset-end $offset_end ${@:2}"
-echo "Running: $bin_graph_cmd"
-$($bin_graph_cmd)
+cmd='./bin-graph'
+args=(--offset-start "$offset_start" --offset-end "$offset_end" "${@:2}")
+echo "Running: ${cmd} ${args[*]}"
+$cmd "${args[@]}"
