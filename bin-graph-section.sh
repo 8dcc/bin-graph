a25627b3ba578ff571d6c4beda77aadb56c684df
8dcc <8dcc.git@gmail.com>
2025-06-21 16:50:07 +0200

    Add GPL license comments
    Add 'set -e' to 'bin-graph-section.sh'.

--- bin-graph-section.sh
+++ bin-graph-section.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
-
+#
+# Copyright 2025 8dcc. All Rights Reserved.
+#
+# This file is part of bin-graph.
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ------------------------------------------------------------------------------
+#
 # Parse the output of `readelf', and use the offsets of the specified section
 # as arguments for `bin-graph'.
+set -e
 
 if [ $# -lt 3 ]; then
     echo "Usage: $(basename "$0") SECTION [OPTION...] INPUT OUTPUT.png" 1>&2
